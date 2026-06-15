c0d9a8dec3e31d5912b0e65196452f5b25ec74ff
Israel <isekundayo700@gmail.com>
2026-06-15 14:57:08 +0100

    finshed working on the hero section of the home page

--- planit/src/Pages/Landing-page/HomePage.jsx
+++ planit/src/Pages/Landing-page/HomePage.jsx
@@ -6,7 +6,7 @@ const HomePage = () => {
   return (
     <>
       {/* Hero Section */}
-      <div className="hero-section relative p-2 md:p-5 lg:p-10 md:max-h-screen">
+      <div className="hero-section relative p-2 md:p-5 lg:p-10">
         {/* Background Image */}
         <img
           src="/images/bg-image.jpg"
@@ -42,13 +42,39 @@ const HomePage = () => {
             </div>
           </header>
 
-          <div className="main text-white p-2 md:p-4 flex md:gap-12 xl:gap-16 items-center">
+          <div className="main text-white p-2 md:p-4 flex gap-12 xl:gap-16 items-center">
             <div className="main-content w-11/20">
-              <h2 className="font-bold text-2xl md:text-3xl text-center playfair-display-font">
-                The Smart Event Management <span>Platform</span> for <span>Planners</span> and <span>Vendors</span>
+              <h2 className="font-bold text-2xl md:text-3xl text-center 2xl:text-left playfair-display-font mb-16">
+                The Smart Event Management <span>Platform</span> for{" "}
+                <span>Planners</span> and <span>Vendors</span>
               </h2>
+              <div className="lato-regular text-sm flex flex-col gap-3 mb-12">
+                <p>
+                  PlanIt is an event management platform that helps planners
+                  discover trusted vendors, manage event tasks, and collaborate
+                  in real time - all in one structured workspace.
+                </p>
+                <p>No scattered chats. No guesswork. No chaos.</p>
+              </div>
+              <div className="montserrat-font font-semibold flex flex-col md:flex-row gap-10 mb-16">
+                <button className="btn btn-planner py-4 px-8 rounded-md">
+                  Get Started as a Planner
+                </button>
+                <button className="btn btn-vendor py-4 px-8 rounded-md ">
+                  Join as a Vendor
+                </button>
+              </div>
+              <div className="roadmap montserrat-font font-semibold flex gap-4">
+                <p>Plan Events</p>
+                <p>Find Trusted Vendors</p>
+                <p>Execute Flawlessly</p>
+              </div>
             </div>
-            <img src={HeroImage} alt="hero image" className="w-9/20 border-8 rounded-xl " />
+            <img
+              src={HeroImage}
+              alt="hero image"
+              className="w-9/20 border-8 rounded-xl "
+            />
           </div>
         </div>
       </div>
