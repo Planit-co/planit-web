14171560e706ef2ce7503e49bb2d8db0d484e648
Israel <isekundayo700@gmail.com>
2026-06-20 10:21:56 +0100

    finished working on the desktop view of the problem section

--- planit/src/Pages/Landing-page/ProblemSection.jsx
+++ planit/src/Pages/Landing-page/ProblemSection.jsx
@@ -1,5 +1,57 @@
+import PlannerStrategizing from "../../assets/images/planners-strategizing.svg";
+import VendorStrategizing from "../../assets/images/vendors-strategizing.svg";
+
 const ProblemSection = () => {
-  return <>Problem Section</>
-}
+  return (
+    <section className="problem-section montserrat-font font-semibold pb-3">
+      <nav className="p-2 md:px-5 md:py-4.5 lg:px-10">
+        <ul className="montserrat-font font-semibold flex justify-between">
+          <li>Corporate Events</li>
+          <li>Large-scale Productions</li>
+          <li>Conferences</li>
+          <li>Weddings</li>
+          <li>Celebrations</li>
+        </ul>
+      </nav>
+
+      <div>
+        <div className="grid grid-cols-2 gap-x-6 gap-y-8 my-15 mx-2 md:mx-5 lg:mx-10">
+          <div>
+            <li className="playfair-display-font font-bold text-xl md:text-2xl mb-7">
+              The Planner’s Struggle
+            </li>
+            <ul className="montserrat-font flex flex-col gap-4">
+              <li>Keeping up with endless Whatsapp threads.</li>
+              <li>Manually sourcing for Vendors.</li>
+              <li>Unverified service providers.</li>
+              <li>Scattered task tracking.</li>
+              <li>Last-minute confusion.</li>
+            </ul>
+          </div>
+
+          <img src={VendorStrategizing} alt="vendors strategizing" />
+
+          <img src={PlannerStrategizing} alt="planners strategizing" />
+
+          <div className="mx-auto">
+            <li className="playfair-display-font font-bold text-xl md:text-2xl mb-7">
+              The Vendor’s Hurdle
+            </li>
+            <ul className="montserrat-font flex flex-col gap-4">
+              <li>Disorganized Client communication.</li>
+              <li>Overdependence on referrals.</li>
+              <li>No structured review system.</li>
+              <li>Limited visibility.</li>
+            </ul>
+          </div>
+        </div>
+
+        <p className="montserrat-font italic font-medium text-xl w-max mx-auto pb-2.5 border-b border-[#D4AF37] ">
+          PlanIt replaces disorganization with structured collaboration!{" "}
+        </p>
+      </div>
+    </section>
+  );
+};
 
 export default ProblemSection;
